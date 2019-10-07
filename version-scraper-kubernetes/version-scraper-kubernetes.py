ca5e08f98a77e83bc47ff1cebdfa5d2a3e9e0083
pythonredhat <pythonredhat1987@gmail.com>
2019-10-07 09:25:21 -0400

    Add functionality for put to web api

--- version-scraper-kubernetes/version-scraper-kubernetes.py
+++ version-scraper-kubernetes/version-scraper-kubernetes.py
@@ -2,6 +2,7 @@ from github import Github
 import requests
 import json
 
+#part 1: get the lastest version from github api
 url = 'https://api.github.com/repos/kubernetes/kubernetes/releases/latest'
 
 response = requests.get(url=url)
@@ -9,6 +10,32 @@ response = requests.get(url=url)
 #json data becomes a dictionary
 json_data = json.loads(response.text)
 
-latest_version = (json_data["name"])
+github_latest_version = (json_data["name"])
 
-print(latest_version)
+print(f"Latest version on Github is {github_latest_version}")
+
+#part 2: get the current version in the web api
+weburl= 'http://localhost:8000/api/v1/version_lord'
+kubernetes_url= 'http://localhost:8000/api/v1/version_lord/5'
+
+response2 = requests.get(url=weburl)
+
+json_data2 = json.loads(response2.text)
+
+for item in json_data2:
+    if item['software'] == "Kubernetes":
+        version_lord_latest_version = item['current_version']
+        print(f"Current version on Version Lord is {version_lord_latest_version}")
+        data = {'current_version': github_latest_version, 'software': 'Kubernetes'}
+        requests.put(url=kubernetes_url, data=data)
+        payload = requests.put(url=kubernetes_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+        print (payload.status_code)
+        print(payload.content)
+        #print(f"New Version Lord value is {github_latest_version}")
+
+
+#part 3: compare and make a decision
+
+#part 4: post if you need to
+
+
